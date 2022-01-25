7c12d1f2f9742bead211c8ea02bc3eb0e0a70858
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-01-24 20:12:13 -0600

    setting up the PORT and express

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-//Express middleware 
+//Express middleware, i'm not sure what this is?
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+//Handle user requests that aren't supported by the app 
+app.use((req, res) => {
+    res.status(404).end();
+});
 
 //start the express server on port 3001 
 app.listen(PORT, () => {
