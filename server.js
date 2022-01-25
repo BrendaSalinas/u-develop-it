ccac36781fcb4faacfc2bbcdf6f8672e98afa384
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-01-25 07:16:08 -0600

    create a POST route to add a candidate, testing with insomnia

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const inputCheck = require('./utils/inputCheck');
 
 //Express middleware, *i'm not sure what this is?*
 
@@ -55,7 +56,7 @@ app.get('/api/candidate/:id', (req, res) => {
     });
 });
 
-//Delete a candidate using the express server and MySQL
+//DELETE a candidate using the express server and MySQL
 app.delete('/api/candidate/:id', (req,res) => {
     const sql = `DELETE FROM candidates WHERE id = ? `;
     const params = [req.params.id];
@@ -77,17 +78,32 @@ app.delete('/api/candidate/:id', (req,res) => {
     });
 });
 
+//POST - create a candidate using express *What does the body part do? *
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if(errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    //Adding a candidate back to the table using MySQL
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
 
 //Adding a candidate back to the table 
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
-const params = [1, "Ronald", "Firbank", 1];
 
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
 
 
 //Handle user requests that aren't supported by the app 
