56babcbed5920ef7dc1bba0dc0e0706820697233
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-01-25 06:03:39 -0600

    handle request that aren't supported by the app

--- server.js
+++ server.js
@@ -3,23 +3,28 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-//Express middleware, i'm not sure what this is?
+//Express middleware, *i'm not sure what this is?*
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
-//connect the application to the MySQL database 
+//connect the application to the MySQL database *is it important the location of this code?*
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        //Your MySQL username,
+        //My MySQL username,
         user: 'root',
-        //Your MySQL password
+        //My MySQL password
         password: 'Iamenough2022@',
         database: 'election'
     },
     console.log('Connected to the election database.')
-)
+);
+
+//return all data from candidates table 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //Handle user requests that aren't supported by the app 
 app.use((req, res) => {
